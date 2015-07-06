b8f1582dbd86559d20f7249987c7010fafa3c9b1
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2015-07-06 20:39:12 +0200

    Jinja2 env is more mockable.

--- src/implugin/jinja2/requestable.py
+++ src/implugin/jinja2/requestable.py
@@ -7,4 +7,7 @@ class Jinja2Requestable(Requestable):
 
     @property
     def jinja2(self):
+        return self._get_jinja2_envoritment()
+
+    def _get_jinja2_envoritment(self):
         return self.registry.queryUtility(IJinja2Environment, name='.jinja2')
